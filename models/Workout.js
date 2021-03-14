e01d983751de966f9ae2c1ed7228c050546dae45
Thoko <thokozile.soko@yahoo.com>
2021-03-14 19:13:17 -0400

    Finished my schema

--- models/Workout.js
+++ models/Workout.js
@@ -2,15 +2,28 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSeed = new Schema({
+const workoutSchema = new Schema({
   day: {
     type: Date,
+    default: () => new Date(),
   },
-  exercises: {
-    type: String,
-  },
+  exercises: [
+    {
+      name: {
+        type: String,
+        trim: true,
+        require: "You need a valid exercise name",
+      },
+      type: String,
+      duration: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number,
+      distance: Number
+    },
+  ],
 });
 
-const Workout = mongoose.model("Workout", workoutSeed);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
