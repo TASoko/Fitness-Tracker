a897ece746fdbe8258157a8ecc51119329025271
Thoko <thokozile.soko@yahoo.com>
2021-03-15 12:47:56 -0400

    I've added all the api routes but I need help making the information render. Also not sure about if the routes are supposed to match the html.

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,18 +3,7 @@ const db = require("../models");
 const app = express();
 
 
-// app.post("/submit", (req, res) => {
-//   console.log(req.body);
-
-//   db.workouts.insert(req.body, (error, data) => {
-//     if (error) {
-//       res.send(error);
-//     } else {
-//       res.send(data);
-//     }
-//   });
-// });
-
+//are the / supposed to match the html?
 app.get("/all", (req, res) => {
     db.Workout.find({})
       .then((dbWorkout) => {
@@ -23,77 +12,89 @@ app.get("/all", (req, res) => {
       .catch((err) => {
         res.json(err);
       });
-
-  // db.Workout.find({}, (error, data) => {
-  //   if (error) {
-  //     res.send(error);
-  //     console.log("It didn't work");
-  //   } else {
-  //     res.json(data);
-  //     console.log("something is working");
-  //   }
-  // });
 });
 
-// app.get("/find/:id", (req, res) => {
-//   db.workouts.findOne(
-//     {
-//       _id: mongojs.ObjectId(req.params.id),
-//     },
-//     (error, data) => {
-//       if (error) {
-//         res.send(error);
-//       } else {
-//         res.send(data);
-//       }
-//     }
-//   );
-// });
+app.get("/find/:id", (req, res) => {
+  db.Workout.findOne(
+    {
+      _id: mongojs.ObjectId(req.params.id),
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
 
-// app.post("/update/:id", (req, res) => {
-//   db.workouts.update(
-//     {
-//       _id: mongojs.ObjectId(req.params.id),
-//     },
-//     {
-//       $set: {
-//         title: req.body.title,
-//         note: req.body.note,
-//         modified: Date.now(),
-//       },
-//     },
-//     (error, data) => {
-//       if (error) {
-//         res.send(error);
-//       } else {
-//         res.send(data);
-//       }
-//     }
-//   );
-// });
+//Is this how you submit a new post request or do I need to specify req.everything?
+app.post("/submit", (req, res) => {
+    db.Workout.insert(req.body, (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    });
+});
 
-// app.delete("/delete/:id", (req, res) => {
-//   db.workouts.remove(
-//     {
-//       _id: mongojs.ObjectID(req.params.id),
-//     },
-//     (error, data) => {
-//       if (error) {
-//         res.send(error);
-//       } else {
-//         res.send(data);
-//       }
-//     }
-//   );
-// });
+//I don't understand why update is crossed out
+app.post("/update/:id", (req, res) => {
+  db.Workout.update(
+    {
+      _id: mongojs.ObjectId(req.params.id),
+    },
+    {
+      $set: {
+        day: req.body.day,
+        exercise: [
+          {
+            name: req.body.name,
+            type: req.body.type,
+            duration: req.body.duration,
+            weight: req.body.weight,
+            reps: req.body.reps,
+            sets: req.body.sets,
+            distance: req.body.distance,
+            modified: Date.now(),
+          },
+        ],
+      },
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
 
-// app.delete("/clearall", (req, res) => {
-//   db.workouts.remove({}, (error, response) => {
-//     if (error) {
-//       res.send(error);
-//     } else {
-//       res.send(response);
-//     }
-//   });
-// });
+app.delete("/delete/:id", (req, res) => {
+  db.Workout.remove(
+    {
+      _id: mongojs.ObjectID(req.params.id),
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
+
+app.delete("/clearall", (req, res) => {
+  db.Workout.remove({}, (error, response) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(response);
+    }
+  });
+});
 module.exports = app
