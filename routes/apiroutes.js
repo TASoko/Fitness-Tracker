3dd30e60665d7bd7d40ae4298103fee39aa4bc87
Thoko <thokozile.soko@yahoo.com>
2021-03-17 17:43:10 -0400

    Iadded comments to the apiroutes file.

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,8 +1,10 @@
+//The depedency
 const db = require("../models");
 
 module.exports = (app) => {
-  //The first function wants me to get everything so findall for api/workouts
+  //The first function wants me to get everything for api/workouts
   // Aggregated workout duration
+  // On the read me we need to be able to not only display the workout but also to add the total duration of the workouts. Since I already had duration I had to aggregate it to add a field to add the durations I'm collecting
   app.get("/api/workouts", (req, res) => {
     db.Workout.aggregate([
       {
@@ -20,7 +22,8 @@ module.exports = (app) => {
       });
   });
 
-  //route for getWorkoutsInRange function
+  //This is the route for getWorkoutsInRange function
+  //This is used to get the last 7 workouts added into the fitness tracker
   app.get("/api/workouts/range", (req, res) => {
     db.Workout.find({})
       .sort({ day: -1 })
@@ -33,7 +36,8 @@ module.exports = (app) => {
       });
   });
 
-  //route for createWorkout
+  //This route is for the createWorkout function. 
+  //This is api route is called when a new workout is created
   app.post("/api/workouts", (req, res) => {
     db.Workout.create(req.body)
       .then((dbWorkout) => {
@@ -44,7 +48,8 @@ module.exports = (app) => {
       });
   });
 
-  //route for addExercise
+  //This route is for addExercise
+  //This is used to put a new workout using its id 
   app.put("/api/workouts/:id", (req, res) => {
     db.Workout.findByIdAndUpdate(req.params.id, {
       $push: { exercises: req.body },
